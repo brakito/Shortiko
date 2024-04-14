1de412f70327c6dabfb0cc038c079271fe25c2ee
Brakito <arangobrmst@gmail.com>
2024-04-14 16:16:42 -0500

    Fix: connect the real database

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-// let connection = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: "shortikodb_accurateno"
-// });
-
 let connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "express_short_link"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "shortikodb_accurateno"
 });
+
+// let connection = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "express_short_link"
+// });
 
 module.exports = connection;
